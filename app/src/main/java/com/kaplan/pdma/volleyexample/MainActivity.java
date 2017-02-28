eda2aa9bbc2cc59e0f774e86f7dd9cefbe144d59
William Ku <kucheech@gmail.com>
2017-02-28 22:17:36 +0800

    processed the JSON response

--- app/src/main/java/com/kaplan/pdma/volleyexample/MainActivity.java
+++ app/src/main/java/com/kaplan/pdma/volleyexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.kaplan.pdma.volleyexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.StringBuilderPrinter;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -9,6 +10,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,7 +28,29 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        textView.setText("Response: " + response.toString());
+                        try {
+                            JSONObject rates = response.getJSONObject("rates");
+                            double aud = rates.getDouble("AUD");
+                            double myr = rates.getDouble("MYR");
+                            double php = rates.getDouble("PHP");
+                            double cny = rates.getDouble("CNY");
+                            double krw = rates.getDouble("KRW");
+                            double thb = rates.getDouble("THB");
+
+                            StringBuilder sb = new StringBuilder();
+                            sb.append("Australia Dollar: " + aud +"\n");
+                            sb.append("Chinese Yuan: " + cny +"\n");
+                            sb.append("Korean Won: " + krw +"\n");
+                            sb.append("Malaysia Ringgit: " + myr +"\n");
+                            sb.append("Philippines Peso: " + php +"\n");
+                            sb.append("Thai Baht: " + thb +"\n");
+                            textView.setText(sb.toString());
+
+
+                        } catch (JSONException e) {
+                            textView.setText(e.getMessage());
+                        }
+
                     }
                 }, new Response.ErrorListener() {
 
